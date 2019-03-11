d92f2f5738f50ed73ee5c5b51dd8660bfe475935
Peter Murphy <peter.francis.murphy@gmail.com>
2019-03-11 08:07:02 -0400

    :construction: Adds a lot of changes
    Should have done more intermediate commits, but I didn't

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React, { setState } from "react"
+import React, { useState } from "react"
 import { InMemoryCache } from "apollo-boost"
 import { ApolloClient } from "apollo-client"
 import { createHttpLink } from "apollo-link-http"
 import { ApolloProvider, Query } from "react-apollo"
+
+import ProfileView from "./components/ProfileView"
 import languagesQuery from "./queries/languages"
+
+import "./App.scss"
 
 const headers = {
   authorization: `Bearer ${process.env.REACT_APP_GH_KEY}`
@@ -18,36 +22,60 @@ const client = new ApolloClient({
   cache: new InMemoryCache().restore(window.__APOLLO_STATE__)
 })
 
-const App = () => (
-  <ApolloProvider client={client}>
-    <div className="App">
-      <Query
-        query={languagesQuery}
-        variables={{
-          login: "ptrfrncsmrph"
-        }}
-      >
-        {({ data, loading, error }) =>
-          loading ? (
-            <>
-              <h3>LOADING...</h3>
-              <p>Please bear with us, we are doing all that we can</p>
-            </>
-          ) : error ? (
-            <>
-              <h3>ERROR</h3>
-              <pre>{JSON.stringify(error, null, 2)}</pre>
-            </>
-          ) : (
-            <>
-              <h3>SUCCESSS</h3>
-              <pre>{JSON.stringify(data, null, 2)}</pre>
-            </>
-          )
-        }
-      </Query>
-    </div>
-  </ApolloProvider>
+const UsernameForm = ({ handleSubmit, handleChange, username }) => (
+  <form onSubmit={handleSubmit}>
+    <input
+      className="form-control"
+      value={username}
+      onChange={handleChange}
+      type="text"
+      placeholder="Enter a GitHub username"
+      aria-label="Search by GitHub username"
+    />
+    <button className="btn">Go</button>
+  </form>
 )
+
+const App = () => {
+  const [username, setUsername] = useState("")
+  const [submitted, setSubmitted] = useState(false)
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    setSubmitted(true)
+  }
+
+  const handleChange = ({ target: { value } }) => {
+    setUsername(value)
+  }
+
+  return (
+    <ApolloProvider client={client}>
+      <div className="App">
+        <UsernameForm
+          handleSubmit={handleSubmit}
+          handleChange={handleChange}
+          username={username}
+        />
+        {submitted && (
+          <Query query={languagesQuery} variables={{ login: username }}>
+            {({ data, loading, error }) =>
+              loading ? (
+                <h3>LOADING...</h3>
+              ) : error ? (
+                <>
+                  <h3>ERROR</h3>
+                  <pre>{JSON.stringify(error, null, 2)}</pre>
+                </>
+              ) : (
+                <ProfileView data={data} />
+              )
+            }
+          </Query>
+        )}
+      </div>
+    </ApolloProvider>
+  )
+}
 
 export default App
---
+++ src/components/ProfileView.js
@@ -0,0 +1,82 @@
+import React from "react"
+import { VictoryPie, VictoryChart, VictoryBar, VictoryTheme } from "victory"
+import formatDistance from "date-fns/formatDistance"
+
+import "./ProfileView.scss"
+
+const languageWeight = data => {
+  const { repositories } = data.user
+  const _byLanguage = repositories.nodes
+    .filter(({ isFork }) => !isFork)
+    .flatMap(({ languages }) =>
+      languages.edges.flatMap(e => ({
+        language: e.node.name,
+        color: e.node.color,
+        size: e.size
+      }))
+    )
+
+  const _sizeByLang = _byLanguage.reduce(
+    (byLang, { language, size }) =>
+      Object.assign({}, byLang, { [language]: (byLang[language] || 0) + size }),
+    {}
+  )
+
+  const _colorByLang = _byLanguage.reduce(
+    (byLang, { language, color }) =>
+      Object.assign({}, byLang, { [language]: color }),
+    {}
+  )
+
+  const byLanguage = Object.entries(_sizeByLang).map(([language, size]) => ({
+    language,
+    size
+  }))
+  return byLanguage
+}
+
+const sumProp = prop => xs => xs.reduce((acc, x) => acc + x[prop], 0)
+
+const ProfileView = ({ data }) => {
+  const totalSize = sumProp("size")(languageWeight(data))
+  return (
+    <>
+      <span className="rate-limit">
+        Rate limit remainder: {data.rateLimit.remaining}
+      </span>
+      <section className="profile">
+        <img className="avatar" src={data.user.avatarUrl} />
+        <h2>{data.user.name}</h2>
+        <span className="created-at">
+          Member since{" "}
+          {formatDistance(new Date(data.user.createdAt), new Date())} ago.
+        </span>
+        <ul>
+          {languageWeight(data).map(({ language, size }) => (
+            <li>
+              <strong>{language}:</strong>{" "}
+              {((size * 100) / totalSize).toFixed(2)}%
+            </li>
+          ))}
+        </ul>
+        <VictoryChart theme={VictoryTheme.material} domainPadding={10}>
+          <VictoryBar
+            data={languageWeight(data).map(({ language, size }) => ({
+              x: language,
+              y: size
+            }))}
+          />
+        </VictoryChart>
+        <VictoryPie
+          theme={VictoryTheme.material}
+          data={languageWeight(data).map(({ language, size }) => ({
+            x: language,
+            y: size
+          }))}
+        />
+      </section>
+    </>
+  )
+}
+
+export default ProfileView
